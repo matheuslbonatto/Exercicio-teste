94234fded4a9cb5dfc5728a3c4b424e72f23b945
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-04-23 21:57:56 -0300

    novo commit

---
+++ src/ExercicioFor3.java
@@ -0,0 +1,20 @@
+public class ExercicioFor3 {
+    public static void main(String[] args) {
+        /*3. Utilizando (For), construir um programa que apresente todos os valores numéricos divisíveis por 4 e menores que 200.
+    Sugestão: a variável que controla o contador do laço deve ser iniciada com valor 1. */
+   
+
+    for(int contador = 0; contador < 200; contador++)
+    if (contador % 4 == 0) {
+        System.out.println(contador);
+        
+    }
+
+
+
+
+
+
+}
+    
+}
